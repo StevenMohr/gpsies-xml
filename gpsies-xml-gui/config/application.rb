9678591aef868691bdfeffde13a726d305a876ef
Hendrik Bergunde <hbergunde@gmx.de>
2012-06-19 00:26:15 +0200

    sparql lib should be autoloaded

--- gpsies-xml-gui/config/application.rb
+++ gpsies-xml-gui/config/application.rb
@@ -23,7 +23,7 @@ module GpsiesXmlGui
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
-	# config.autoload_paths += "#{config.root}/app/models"
+	config.autoload_paths += "#{config.root}/../sparql"
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
