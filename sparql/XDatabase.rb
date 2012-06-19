4bebd758e1cc5140bda1eee2da4c994b1252a3a3
rschaden <rschaden@gmail.com>
2012-06-19 23:20:44 +0200

    Reorganized XDatabase class

--- sparql/XDatabase.rb
+++ sparql/XDatabase.rb
@@ -7,7 +7,12 @@ require 'queryhelper.rb'
 class XDatabase
   
   def initialize(host,port,username,password)
-    @session = BaseXClient::Session.new(host, port, username, password)
+    begin
+      @session = BaseXClient::Session.new(host, port, username, password)
+    rescue Exception => e
+      puts e
+      result = nil
+    end
   end
   
   def open(db)
@@ -48,15 +53,43 @@ class XDatabase
     return result
   end
   
-  def get_waypoints(id,db)
+  
+   #for a given ID gets waypoints from database,
+   #fetches POIs from sparql
+   #inserts them into our XML-database
+   #returns POIs as XML string
+   def fetch_POIs_from_SPARQL(id)
+     
+      points = get_waypoints(id)
+      #take only each 10th point:
+      coords = create_coord_array(points,10)      
+      result = get_POIs(coords)
+          
+      if !result.nil?
+        res = insert_pois(id, result)
+        if !res.nil?
+          pois = "<pois>#{result}</pois>"
+        else
+          pois=nil
+        end
+      end
+   end
+  
+  def close
+    @session.close
+  end
+  
+  private
+  
+    def get_waypoints(id)
     begin 
       input = "for $x in track    
       where $x/uid = \"#{id}\"
       return $x/waypoints"
     
-      if !self.open(db)
-        raise "Error while opening database"
-      end
+      #if !self.open(db)
+        #raise "Error while opening database"
+      #end
       result = self.query(input)
     rescue Exception => e
       # print exception
@@ -69,11 +102,11 @@ class XDatabase
     return result
   end
   
-  def insert_pois(id, pois, db)
+  def insert_pois(id, pois)
     begin
-      if !self.open(db)
-        raise "Error while opening database"
-      end
+      #if !self.open(db)
+        #raise "Error while opening database"
+      #end
       
       queryString = "xquery for $x in track where $x/uid = \"#{id}\"
         return (            
@@ -107,28 +140,6 @@ class XDatabase
     # close session
     #self.close
     return result
-  end
-  
-   #for a given ID gets waypoints from database,
-   #fetches POIs from sparql
-   #inserts them into our XML-database
-   #returns POIs as XML string
-   def fetch_POIs_from_SPARQL(id,db)
-     
-   points = get_waypoints(id, db)
-   coords = create_coord_array(points,10)      
-   result = get_POIs(coords)
-      
-   if !result.nil?
-     res = insert_pois(id, result, db)
-     #TODO: define what happens if insert into database is not successful
-        
-     pois = "<pois>#{result}</pois>"
-     end
-   end
-  
-  def close
-    @session.close
   end
 end 
   
